07508d3ec43b2c92312658843e472168edb975ab
Lauren Hallau <lhallau@gmail.com>
2020-01-09 14:26:16 -0600

    starting on javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+//Generate Random Password
+function generate () {
+    var passwordLength = prompt("How long do you want your password to be? Enter a number between 8 and 128.");
+        if (passwordLength > 8 || passwordLength > 128) {
+            alert("Please enter a number between 8 and 128!")
+        }
+        
+}
+
+//declaring variables
+
+var SpecialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var NumericalChars = "0123456789";
+var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+//Determining the characters the user wants
+
+var SpecialChars = confirm("Would you like to include special characters in your password?");
+var NumericalChars = confirm("Would you like to include numbers in your password?");
+var lowerCaseChars = confirm("Would you like to include lower case letters in your password?");
+var upperCaseChars = confirm("Would you like to include upper case letters in your password?");
