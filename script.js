b76ba09fd5113a03b46faef521abda36fd0318b9
Lauren Hallau <lhallau@gmail.com>
2020-01-10 13:44:34 -0600

    for loop changes

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 //Start by defining the length of user password
 var passwordLength = prompt("How long do you want your password to be? Enter a number between 8 and 128."); 
-    console.log("Your password is" + passwordLength);
+console.log("Your password is" + passwordLength);
     if (passwordLength < 8|| passwordLength > 128) {
         alert("Please enter a number between 8 and 128!");
 }
 
 //Determining the characters the user wants
 var wantSpecial = confirm("Would you like to include special characters in your password?");
-    console.log(wantSpecial);
+console.log(wantSpecial);
 
 var wantNumber = confirm("Would you like to include numbers in your password?");
-    console.log(wantNumber);
+console.log(wantNumber);
 
 var wantLower = confirm("Would you like to include lower case letters in your password?");
-    console.log(wantLower);
+console.log(wantLower);
 
 var wantUpper = confirm("Would you like to include upper case letters in your password?");
-    console.log(wantUpper);
+console.log(wantUpper);
 
 
 //Generate Random Password
 function generate() {
 
     //declaring variables
+    var all = ' ';
     var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     var numericalChars = "0123456789";
     var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
     var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var all = '';
+    var password = " ";
+    
 
 //if statements
 if (wantSpecial === true) {
@@ -42,31 +44,17 @@ if (wantLower === true) {
 if (wantUpper === true) {
     all += upperCaseChars;
 }
-if (wantSpecial === false && wantNumber === false && wantLower === false && wantUpper === false) {
+else if (wantSpecial === false && wantNumber === false && wantLower === false && wantUpper === false) {
     alert("Please select at least one type of character!");
     return null;
 }
 
-var password = ''
-    for(var i = 0; i < parseInt(passwordLength); i++) {
+    for(var i = 0; i < passwordLength; i++) {
         var randomPick = all[Math.floor(Math.random() * all.length)]
         password += randomPick;
-    }
-    console.log("New Random Password", password);
-
+        }
+        console.log(password);
         var blankInput = document.querySelector("#displayPass");
         blankInput.value = password;
-}
-
-
-    
-
-
-
-
-
-
-
-
-
-
+    }
+    generate ()
