3d604b7d30d1f486d459793db9ba3df460ae3bc1
Lauren Hallau <lhallau@gmail.com>
2020-01-10 12:52:07 -0600

    troubleshooting console.log

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 //Start by defining the length of user password
 var passwordLength = prompt("How long do you want your password to be? Enter a number between 8 and 128."); 
-
-if (passwordLength < 8|| passwordLength > 128) {
-    alert("Please enter a number between 8 and 128!");
+    console.log("Your password is" + passwordLength);
+    if (passwordLength < 8|| passwordLength > 128) {
+        alert("Please enter a number between 8 and 128!");
 }
 
 //Determining the characters the user wants
-var specialChars = confirm("Would you like to include special characters in your password?");
-var numericalChars = confirm("Would you like to include numbers in your password?");
-var lowerCaseChars = confirm("Would you like to include lower case letters in your password?");
-var upperCaseChars = confirm("Would you like to include upper case letters in your password?");
+var wantSpecial = confirm("Would you like to include special characters in your password?");
+    console.log(wantSpecial);
+
+var wantNumber = confirm("Would you like to include numbers in your password?");
+    console.log(wantNumber);
+
+var wantLower = confirm("Would you like to include lower case letters in your password?");
+    console.log(wantLower);
+
+var wantUpper = confirm("Would you like to include upper case letters in your password?");
+    console.log(wantUpper);
+
 
 //Generate Random Password
-function generate () {
+function generate() {
 
     //declaring variables
-var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var numericalChars = "0123456789";
-var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var newPassword = " ";
+    var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    var numericalChars = "0123456789";
+    var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var all = '';
 
 //if statements
-if (specialChars === true) {
+if (wantSpecial === true) {
     all += specialChars;
 }
-if (numericalChars === true) {
+if (wantNumber === true) {
     all += numericalChars;
 }
-if (lowerCaseChars === true) {
+if (wantLower === true) {
     all += lowerCaseChars;
 }
-if (upperCaseChars === true) {
+if (wantUpper === true) {
     all += upperCaseChars;
 }
+if (wantSpecial === false && wantNumber === false && wantLower === false && wantUpper === false) {
+    alert("Please select at least one type of character!");
+    return null;
+}
+
+var password = ''
+    for(var i = 0; i < parseInt(passwordLength); i++) {
+        var randomPick = all[Math.floor(Math.random() * all.length)]
+        password += randomPick;
+    }
+    console.log("New Random Password", password);
+
+        var blankInput = document.querySelector("#displayPass");
+        blankInput.value = password;
 }
 
 
