1602831d2848c82cefbdd9761854006f2d80ce71
Lauren Hallau <lhallau@gmail.com>
2020-01-09 21:20:19 -0600

    if statments in javascript

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ if (passwordLength < 8|| passwordLength > 128) {
 }
 
 //Determining the characters the user wants
-var SpecialChars = confirm("Would you like to include special characters in your password?");
-var NumericalChars = confirm("Would you like to include numbers in your password?");
+var specialChars = confirm("Would you like to include special characters in your password?");
+var numericalChars = confirm("Would you like to include numbers in your password?");
 var lowerCaseChars = confirm("Would you like to include lower case letters in your password?");
 var upperCaseChars = confirm("Would you like to include upper case letters in your password?");
 
@@ -15,16 +15,32 @@ var upperCaseChars = confirm("Would you like to include upper case letters in yo
 function generate () {
 
     //declaring variables
-var SpecialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var NumericalChars = "0123456789";
+var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numericalChars = "0123456789";
 var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var newPassword = "";
+var newPassword = " ";
 
 //if statements
-
-
+if (specialChars === true) {
+    all += specialChars;
 }
+if (numericalChars === true) {
+    all += numericalChars;
+}
+if (lowerCaseChars === true) {
+    all += lowerCaseChars;
+}
+if (upperCaseChars === true) {
+    all += upperCaseChars;
+}
+}
+
+
+    
+
+
+
 
 
 
